77b15dfeeb30714d4b4721ff2465a1aa517b01e3
napassorn29 <bubbub4388@gmail.com>
2023-09-15 15:06:00 +0700

    file turtlesim_plus

---
+++ build/turtlesim_plus_interfaces/turtlesim_plus_interfaces__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/napassorn/Desktop/FRA501_exam1_6435_6449/build/turtlesim_plus_interfaces/turtlesim_plus_interfaces__py
+# Build directory: /home/napassorn/Desktop/FRA501_exam1_6435_6449/build/turtlesim_plus_interfaces/turtlesim_plus_interfaces__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
